03e67beffefbf7a16a8423ef3c42b55f3a6a502b
FIRST_NAME LAST_NAME <example@example.com>
2024-04-17 16:48:11 -0500

    changes made

--- travel_wishlist/forms.py
+++ travel_wishlist/forms.py
@@ -1,7 +1,20 @@
 from django import forms
+from django.forms import FileInput, DateInput
 from .models import Place
 
 class NewPlaceForm(forms.ModelForm):
     class Meta:
         model = Place
         fields = ('name', 'visited')
+
+# create custom date input field outherwise would get a plain text field
+class DateInput(forms.DateInput):
+    input_type = 'date' # override the dafault input type, which is a text
+
+class TripReviewForm(forms.ModelForm):
+    class Meta:
+        model = Place
+        fields = ('notes', 'date_visited', 'photo')
+        widgets = {
+            'date_visited': DateInput()
+        }
